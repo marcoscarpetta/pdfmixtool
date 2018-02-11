36718dc5f453124dbc71ca1703f4d40520339542
Marco Scarpetta <marcoscarpetta02@gmail.com>
2018-02-11 12:03:21 +0100

    Update translations

--- languages/pdfmixtool_it.ts
+++ languages/pdfmixtool_it.ts
@@ -6,60 +6,62 @@
     <message>
         <location filename="../src/aboutdialog.cpp" line="34"/>
         <source>About PDF Mix Tool</source>
-        <translation>Informazioni su PDF Mix Tool</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/aboutdialog.cpp" line="44"/>
         <source>Close</source>
-        <translation>Chiudi</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/aboutdialog.cpp" line="54"/>
         <source>Version %1</source>
-        <translation>Versione %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/aboutdialog.cpp" line="66"/>
         <source>An application to split, merge, rotate and mix PDF files.</source>
-        <translation>Un&apos;applicazione per dividere, unire, ruotare e mescolare file PDF.</translation>
-    </message>
-    <message>
-        <location filename="../src/aboutdialog.cpp" line="74"/>
-        <source>About</source>
-        <translation>Informazioni</translation>
-    </message>
-    <message>
-        <location filename="../src/aboutdialog.cpp" line="90"/>
-        <source>License</source>
-        <translation>Licenza</translation>
-    </message>
-    <message>
-        <location filename="../src/aboutdialog.cpp" line="106"/>
-        <source>Changelog</source>
-        <translation>Changelog</translation>
-    </message>
-    <message>
-        <source>Version %1
-
-</source>
-        <translation type="vanished">Versione %1
-
-</translation>
-    </message>
-    <message>
-        <source>
-An application to split, merge, rotate and mix PDF files
-
-</source>
-        <translation type="vanished">
-Un&apos;applicazione per dividere, unire, ruotare e mescolare file PDF
-
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/aboutdialog.cpp" line="67"/>
         <source>Website</source>
-        <translation>Sito web</translation>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="74"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="92"/>
+        <source>License</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="102"/>
+        <source>Submit a pull request</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="103"/>
+        <source>Report a bug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="104"/>
+        <source>Help translating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="111"/>
+        <source>Contribute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/aboutdialog.cpp" line="127"/>
+        <source>Changelog</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -67,22 +69,22 @@ Un&apos;applicazione per dividere, unire, ruotare e mescolare file PDF
     <message>
         <location filename="../src/comboboxdelegate.cpp" line="38"/>
         <source>No rotation</source>
-        <translation>Non ruotare</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/comboboxdelegate.cpp" line="39"/>
         <source>90°</source>
-        <translation>90°</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/comboboxdelegate.cpp" line="40"/>
         <source>180°</source>
-        <translation>180°</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/comboboxdelegate.cpp" line="41"/>
         <source>270°</source>
-        <translation>270°</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -90,119 +92,119 @@ Un&apos;applicazione per dividere, unire, ruotare e mescolare file PDF
     <message>
         <location filename="../src/mainwindow.cpp" line="37"/>
         <source>Add PDF file</source>
-        <translation>Aggiungi file PDF</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="38"/>
         <source>Move up</source>
-        <translation>Sposta su</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="39"/>
         <source>Move Down</source>
-        <translation>Sposta giù</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="40"/>
         <source>Remove file</source>
-        <translation>Rimuovi file</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="41"/>
         <source>About</source>
-        <translation>Informazioni</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="42"/>
         <source>Generate PDF</source>
-        <translation>Genera PDF</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="57"/>
         <source>PDF Mix Tool</source>
-        <translation>PDF Mix Tool</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="71"/>
         <location filename="../src/mainwindow.cpp" line="77"/>
         <source>PDF files (*.pdf)</source>
-        <translation>File PDF (*.pdf)</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="89"/>
         <source>Filename</source>
-        <translation>Nome del file</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="90"/>
         <source>Page count</source>
-        <translation>Numero di pagine</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="91"/>
         <source>Pages filter</source>
-        <translation>Filtri sulle pagine</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="92"/>
         <source>Rotation</source>
-        <translation>Rotazione</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="190"/>
         <source>No rotation</source>
-        <translation>Non ruotare</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="299"/>
         <source>Output pages: %1</source>
-        <translation>Pagine in uscita: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="307"/>
         <location filename="../src/mainwindow.cpp" line="327"/>
         <source>PDF generation error</source>
-        <translation>Errore nella generazione del file PDF</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="308"/>
         <source>You must add at least one pdf file!</source>
-        <translation>Devi aggiungere almeno un file PDF!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="328"/>
         <source>&lt;p&gt;The PDF generation failed due to the following errors:&lt;/p&gt;</source>
-        <translation>&lt;p&gt;La generazione del PDF è fallita a causa dei seguenti errori:&lt;/p&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="340"/>
         <source>&lt;li&gt;Invalid character &quot;&lt;b&gt;%1&lt;/b&gt;&quot; in pages filter of file &quot;&lt;b&gt;%2&lt;/b&gt;&quot;&lt;/li&gt;</source>
-        <translation>&lt;li&gt;Carattere non valido &quot;&lt;b&gt;%1&lt;/b&gt;&quot; nel filtro delle pagine del file &quot;&lt;b&gt;%2&lt;/b&gt;&quot;&lt;/li&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="346"/>
         <source>&lt;li&gt;Invalid interval &quot;&lt;b&gt;%1&lt;/b&gt;&quot; in pages filter of file &quot;&lt;b&gt;%2&lt;/b&gt;&quot;&lt;/li&gt;</source>
-        <translation>&lt;li&gt;Intervallo non valido &quot;&lt;b&gt;%1&lt;/b&gt;&quot; nel filtro delle pagine del file &quot;&lt;b&gt;%2&lt;/b&gt;&quot;&lt;/li&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="352"/>
         <source>&lt;li&gt;Boundaries of interval &quot;&lt;b&gt;%1&lt;/b&gt;&quot; in pages filter of file &quot;&lt;b&gt;%2&lt;/b&gt;&quot; are out of allowed interval&lt;/li&gt;</source>
-        <translation>&lt;li&gt;I limiti dell&apos;intervallo &quot;&lt;b&gt;%1&lt;/b&gt;&quot; nel filtro delle pagine del file &quot;&lt;b&gt;%2&lt;/b&gt;&quot; sono oltre i valori consentiti&lt;/li&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="368"/>
         <source>PDF generation warning</source>
-        <translation>Warning nella generazione del PDF</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="369"/>
         <source>&lt;p&gt;The following problems were encountered while generating the PDF file:&lt;/p&gt;</source>
-        <translation>&lt;p&gt;Sono stati riscontrati i seguenti problemi durante la generazione del file PDF:&lt;/p&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/mainwindow.cpp" line="381"/>
         <source>&lt;li&gt;Interval &quot;&lt;b&gt;%1&lt;/b&gt;&quot; in pages filter of file &quot;&lt;b&gt;%2&lt;/b&gt;&quot; is overlapping with another interval&lt;/li&gt;</source>
-        <translation>&lt;li&gt;L&apos;intervallo &quot;&lt;b&gt;%1&lt;/b&gt;&quot; nel filtro delle pagine del file &quot;&lt;b&gt;%2&lt;/b&gt;&quot; è sovrapposto ad un altro intervallo&lt;/li&gt;</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
